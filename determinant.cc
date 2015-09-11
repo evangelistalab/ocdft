26c46ec8c6705fe569e8874652111885d3cf98e0
prakashverma <vermaUFl@gmail.com>
2015-09-11 12:07:41 -0400

    small correction

--- determinant.cc
+++ determinant.cc
@@ -12,6 +12,14 @@ Determinant::Determinant(double energy, SharedMatrix Ca, SharedMatrix Cb, const 
 {
 }
 
+
+Determinant::Determinant(SharedMatrix Ca, SharedMatrix Cb, const Dimension& nalphapi, const Dimension& nbetapi,int fmo,
+    int state_a, int state_b,int vir_frozen)
+    :energy_(0.0),nalphapi_(nalphapi), nbetapi_(nbetapi), Ca_(Ca->clone()), Cb_(Cb->clone()),o_fmo(fmo),v_fmoA(state_a),
+    v_fmoB(state_b),vir_fmo(vir_frozen)
+{
+}
+
 Determinant::Determinant(SharedMatrix Ca, SharedMatrix Cb, const Dimension& nalphapi, const Dimension& nbetapi)
     : energy_(0.0), nalphapi_(nalphapi), nbetapi_(nbetapi), Ca_(Ca->clone()), Cb_(Cb->clone())
 {
@@ -24,6 +32,12 @@ Determinant::Determinant(const Determinant& det)
     Cb_ = det.Cb_->clone();
     nalphapi_ = det.nalphapi_;
     nbetapi_ = det.nbetapi_;
+
+    o_fmo=0;
+    v_fmoA=0;
+    v_fmoB=0;
+    vir_fmo=0;
+
 }
 
 Determinant::~Determinant()
@@ -47,5 +61,13 @@ void Determinant::print()
     nalphapi_.print();
     nbetapi_.print();
 }
+
+
+void Determinant::occup()
+{
+  outfile->Printf("\n|");
+  outfile->Printf(">\n");
+}
+
 
 }} // namespaces
